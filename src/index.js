17f24c2227d9c76bbf7b3d3ab9e5cab3e37ed6d3
Valzhin <70908623+Valzhin@users.noreply.github.com>
2020-10-17 20:39:57 +0300

    init: start brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+    var brackets = Object.fromEntries(bracketsConfig);
+    for (var a of str.split``) {
+        if (arr[0] === a) {
+            arr.shift();
+        } else if (brackets[a]) {
+            arr.unshift(brackets[a]);
+        } else {
+            return false;
+        }
+    }
+
+    return arr.length === 0;
+}
 }
